a42c4056673a6235d46f39a22d0837655880eaf9
yuri <yuricodesbot@gmail.com>
2021-10-01 19:18:29 -0400

    first commit

---
+++ Lab3-Sort.c
@@ -0,0 +1,11 @@
+sort1 uses: Bubble sort
+
+How do you know?: Because it's considerably faster when working on big amounts of data sorted.
+
+sort2 uses: Merge sort
+
+How do you know?: This one it's faster when it has an unordered (random) input list.
+
+sort3 uses: Selection sort
+
+How do you know?: It takes longer with big amounts of data. 
